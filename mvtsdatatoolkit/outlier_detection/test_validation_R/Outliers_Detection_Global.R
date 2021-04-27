751b4748d21832deb03bea034f9c9cbee8817c70
skad00sh <heramblonkar@gmail.com>
2021-04-27 22:38:18 +0530

    Added sorting

--- mvtsdatatoolkit/outlier_detection/test_validation_R/Outliers_Detection_Global.R
+++ mvtsdatatoolkit/outlier_detection/test_validation_R/Outliers_Detection_Global.R
@@ -7,10 +7,10 @@ Outliers_StdDev <- function(df_list, distance_threshold = 3){
   lower_div <- df_mean - (distance_threshold * df_sd)
   
   outliers <- c(df_list[df_list > upper_div], df_list[df_list < lower_div])
-  return(outliers)
+  return(sort(outliers))
 }
 
 # Function to detect outliers using IQR method
 Outliers_IQR <- function(df_list){
-  return(boxplot.stats(df_list[!is.na(df_list)])$out)
+  return(sort(boxplot.stats(df_list[!is.na(df_list)])$out))
 }
