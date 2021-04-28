ceb59eb75e89d1c9439e7e7081515b74133db702
skad00sh <heramblonkar@gmail.com>
2021-04-28 09:32:22 +0530

    Updated IQR code

--- mvtsdatatoolkit/outlier_detection/test_validation_R/Outliers_Detection_Global.R
+++ mvtsdatatoolkit/outlier_detection/test_validation_R/Outliers_Detection_Global.R
@@ -12,5 +12,14 @@ Outliers_StdDev <- function(df_list, distance_threshold = 3){
 
 # Function to detect outliers using IQR method
 Outliers_IQR <- function(df_list){
-  return(sort(boxplot.stats(df_list[!is.na(df_list)])$out))
+  df_list <- df_list[!is.na(df_list)]
+  Q1 <- quantile(df_list, 0.25)
+  Q3 <- quantile(df_list, 0.75)
+  IQR <- Q3 - Q1
+  lower_div <- Q1 - 1.5 * IQR
+  upper_div <- Q3 + 1.5 * IQR
+  
+  outliers <- c(df_list[df_list > upper_div], df_list[df_list < lower_div])
+  return (sort(outliers))
+  #return(sort(boxplot.stats(df_list[!is.na(df_list)])$out))
 }
